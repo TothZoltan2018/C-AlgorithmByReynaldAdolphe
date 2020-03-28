f03a7a429f94a0826191b00e353ddf68b6422103
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-03-28 09:56:57 +0100

    LinkedList - .Net solution examples

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -8,7 +8,81 @@ namespace SinglyLinkedList
 {
     class Program
     {
+        //My solution: these definitions needed to be moved here
+        static string[] words = { "the", "actor", "jumped", "over", "the", "director" };
+        static LinkedList<string> sentence = new LinkedList<string>(words);
+
         static void Main(string[] args)
+        {
+            //HomeMadeLinkedList();
+            DemoLinkedListFeatures();
+        }
+
+        private static void DemoLinkedListFeatures()
+        {
+            //string[] words = { "the", "actor", "jumped", "over", "the", "director"};
+            //LinkedList<string> sentence = new LinkedList<string>(words);
+            Display(sentence, "The linked list values:");
+
+
+            sentence.AddFirst("today");
+            Display(sentence, "Test 1: Add 'today' to beginning of the list:");
+
+            LinkedListNode<string> mark1 = sentence.First;
+            sentence.RemoveFirst();
+            sentence.AddLast(mark1);
+            Display(sentence, "Test 2: Move first node to be the last node:");
+
+            sentence.RemoveLast();
+            sentence.AddLast("yesterday");
+            Display(sentence, "Test 3: Change the last node to 'yesterday'.");
+
+            mark1 = sentence.Last;
+            sentence.RemoveLast();
+            sentence.AddFirst(mark1);
+            Display(sentence, "Test 4: Move the last node to be the first node:");
+
+            sentence.RemoveFirst();
+            LinkedListNode<string> current = sentence.FindLast("the");
+            IndicateNode(current, "Test 5: Indicate last ocurance of 'the' node:");
+
+            sentence.AddAfter(current, "old");
+            sentence.AddAfter(current, "lazy");
+            IndicateNode(current, "Test 6: Add 'lazy' and 'old' after 'the' node.");
+
+            current = sentence.Find("actor");
+            IndicateNode(current, "Test 7: Indicate the 'actor' node:");
+
+            sentence.AddBefore(current, "quick");
+            sentence.AddBefore(current, "skinny");
+            IndicateNode(current, "Test 8: Add 'quick' and 'skinny' before 'actor' node:");
+
+            Console.ReadLine();
+        }
+        //My solution
+        private static void IndicateNode(LinkedListNode<string> current, string v)
+        {
+            string temp = current.Value;
+            current.Value = "(" + current.Value + ")";
+            
+            Display(sentence, v);
+            current.Value = temp;
+        }
+        //My solution
+        private static void Display(LinkedList<string> sentence, string v)
+        {
+            Console.WriteLine(v);
+            foreach (var word in sentence)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+        #region HomeMadeLinkedList
+        private static void HomeMadeLinkedList()
         {
             SinglyLinkedList myList = new SinglyLinkedList();
             myList.InsertFirst(100);
@@ -69,7 +143,7 @@ namespace SinglyLinkedList
             newNode.data = data;
             current.next = newNode;
         }
-    
+
     }
 
     public class Node
@@ -79,7 +153,8 @@ namespace SinglyLinkedList
 
         public void DisplayNode()
         {
-            Console.WriteLine("<" + data + "");
+            Console.WriteLine("<" + data + ">");
         }
-    }
+    } 
+    #endregion
 }
